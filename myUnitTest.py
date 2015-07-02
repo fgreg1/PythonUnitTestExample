1fd63bbf70170115ef2c367750762968b3673a5e
fgreg1 <frank@opensourcearchitect.co>
2015-07-02 16:34:03 -0500

    Update myUnitTest.py

--- myUnitTest.py
+++ myUnitTest.py
@@ -23,5 +23,6 @@ class myUnitTest(unittest.TestCase):
 
 
 if __name__ == '__main__':  
-    unittest.main()  
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports')) 
     
